dfeb69ab518a4827e499039999fbb361b24c35c6
Andrea Rosà <andrea.rosa@usi.ch>
2018-10-18 10:48:22 +0200

    Documentation of gc-filtering.py revised.

--- postprocessing/gc-filtering.py
+++ postprocessing/gc-filtering.py
@@ -5,50 +5,54 @@ import sys
 import csv
 
 '''
-Filters the context switches and the CPU traces, by eliminating the values obtained while GC collection was active.
+Documentation:
+    
+This script filters the CS and the CPU traces, eliminating measurements obtained during GC cycles.
 
-The output are two new traces (named filtered-cs.csv and filtered-cpu.csv by default), containing filtered context switches and CPU measurements respectively.
 
-Usage: ./gc-filtering.py --cs <path to context switches trace> --cpu <path to CPU trace> --gc <path to GC trace> [--outcs <path to filtered context switches trace> --outcpu <path to filtered CPU trace>]
+The script produces two new traces (named 'filtered-cs.csv' and 'filtered-cpu.csv' by default), containing the filtered CS and CPU measurements, respectively.
+
+Usage: ./gc-filtering.py --cs <path to CS trace> --cpu <path to CPU trace> --gc <path to GC trace> [--outcs <path to filtered CS trace> --outcpu <path to filtered CPU trace>]
 
 Parameters:
--> --cs: the context switches trace to be filtered
--> --cpu: the CPU trace to be filtered
--> --gc: the GC trace on which the filtering of context switches and CPU data is based
-Note: files 'path/to/context switches trace', 'path/to/CPU trace', and 'path/to/GC trace' should be produced by the same profiling run.
+-> --cs: the path to the CS trace to be filtered
+-> --cpu: the path to the CPU trace to be filtered
+-> --gc: the path to the GC trace. Filtering of CS and CPU measurements are be based on data contained in this trace
+Note: All input traces should have been produced by tgp with a SINGLE profiling run, either in the bytecode profiling or reference-cycles profiling mode.
+
 Optional parameters:
--> --outcs: the name of trace containing the filtered context switches. If none is provided, then the output trace will be named 'filtered-cs.csv'
--> --outcpu: the name of the trace containing the filtered CPU measurements. If none is provided, then the output trace will be named 'filtered-cpu.csv'
+-> --outcs: the path to the output trace containing the filtered context switches. If none is provided, then the output trace will be produced in './filtered-cs.csv'
+-> --outcpu: the path to the output trace containing the filtered CPU utilization measurements. If none is provided, then the output trace will be produced in './filtered-cpu.csv'
 '''
 
-#Default name of the output filtered context-switches file
+#Default name of the output filtered CS trace
 DEFAULT_CS_OUT_FILE = "filtered-cs.csv"
-#Default name of the output filtered CPU file
+#Default name of the output filtered CPU trace
 DEFAULT_CPU_OUT_FILE = "filtered-cpu.csv"
 
-#The number of fields in the context-switches csv file
+#Number of columns in the CS trace
 FIELDS_CS = 2
-#The number of fields in the CPU csv file
+#Number of columns in the CPU trace
 FIELDS_CPU = 3
-#The number of fields in the GC csv file
+#Number of columns in the GC trace
 FIELDS_GC = 2
 
-#The array containing context-switches data before filtering
+#A list containing context-switches data before filtering
 cs_data_array_bf = []
-#The array containing context-switches data after filtering
+#A list containing context-switches data after filtering
 cs_data_array = []
 
-#The array containing CPU data before filtering
+#A list containing CPU data before filtering
 cpu_data_array_bf = []
-#The array containing CPU data before filtering
+#A list containing CPU data after filtering
 cpu_data_array = []
 
-#The array containing garbage collection data
+#A list containing garbage-collection data
 gc_data_array = []
 
 class CSData:
     '''
-    A class containing context-switches data from the csv file.
+    A class containing context-switches data taken from the CS trace.
     '''
     def __init__(self, timestamp, context_switches):
         self.timestamp = timestamp
@@ -56,7 +60,7 @@ class CSData:
 
 class CPUData:
     '''
-    A class containing CPU data from the csv file.
+    A class containing CPU data taken from the CPU trace.
     '''
     def __init__(self, timestamp, user, system):
         self.timestamp = timestamp
@@ -65,7 +69,7 @@ class CPUData:
 
 class GCData:
     '''
-    A class containing GC data from the csv file.
+    A class containing GC data taken from the GC trace.
     '''
     def __init__(self, start_time, end_time):
         self.start_time = start_time
@@ -74,7 +78,7 @@ class GCData:
 def contains_letters(string):
     '''
     Checks whether the input string contains letters.
-    string: the input string to perform the check on.
+    string: the string on which to check the presence of letters.
     Returns true if the string contains letters, false otherwise.
     '''
     for s in string:
@@ -85,9 +89,9 @@ def contains_letters(string):
 def read_csv(input_csv_file, target_array, data_type, file_delimiter):
     '''
     Reads the input csv file, and sets up the input data structure.
-    Depending on the data type, a new instance of CSData, CPUData, or GCData will be created and added to the target array.
+    Depending on the data type, a new instance of CSData, CPUData, or GCData will be created and added to the target list.
     input_csv_file: the csv file to read.
-    target_array: the array where to write the content of the csv file.
+    target_array: the list where to write the content of the csv file.
     data_type: the data which will be read.
     file_delimiter: the delimiter used by the file.
     '''
@@ -100,7 +104,7 @@ def read_csv(input_csv_file, target_array, data_type, file_delimiter):
             #Reads and writes context-switches data
             if data_type == "CS" and csv_line_counter > 1:
                 if len(row) != FIELDS_CS:
-                    print("Wrong context switches trace format")
+                    print("Wrong CS trace format")
                     exit(0)
                 if row[0][0] != "-" and row[1][0] != "-" and contains_letters(row[0]) == False and contains_letters(row[1]) == False:
                     new_cs = CSData(float(row[0]), float(row[1]))
@@ -129,9 +133,8 @@ def read_csv(input_csv_file, target_array, data_type, file_delimiter):
 
 def filter_cs():
     '''
-    Filters the context-switches array and writes the valid context-switches data into the filtered array.
-    To determine whether a context-switch is valid, said context-switch timestamp is checked against GC start and end timestamps: if the timestamp does not fall in any GC time interval, then the
-    context-switch is valid.
+    Filters the context-switches list and writes the valid CS data into the filtered array.
+    To determine whether a CS measurement is valid, the timestamp of such measurement is checked against all GC start and end timestamps: if the timestamp does not fall in any time interval where GC was active, then the context-switch is valid.
     '''
     for cs_data in cs_data_array_bf:
         found = False
@@ -145,8 +148,7 @@ def filter_cs():
 def filter_cpu():
     '''
     Filters the CPU array and writes the valid CPU data into the filtered array.
-    To determine whether a CPU measurement is valid, said CPU timestamp is checked against GC start and end timestamps: if the timestamp does not fall in any GC time interval, then the
-    CPU measurement is valid.
+    To determine whether a CPU measurement is valid, the timestamp of such measurement is checked against all GC start and end timestamps: if the timestamp does not fall in any time interval where GC was active, then the CPU measurement is valid.
     '''
     for cpu_data in cpu_data_array_bf:
         found = False
@@ -159,7 +161,7 @@ def filter_cpu():
 
 def write_cs_csv():
     '''
-    Writes the filtered context-switches array into a new csv file called filtered-cs.csv.
+    Writes the filtered context-switches list into a new csv file.
     '''
     with open (out_cs_file, 'w') as csvfile:
         fieldnames = ['Timestamp (ns)', 'Context Switches']
@@ -170,7 +172,7 @@ def write_cs_csv():
 
 def write_cpu_csv():
     '''
-    Writes the filtered CPU array into a new csv file called filtered-cpu.csv.
+    Writes the filtered CPU list into a new csv file.
     '''
     with open (out_cpu_file, 'w') as csvfile:
         fieldnames = ['Timestamp (ns)', 'CPU utilization (user)', 'CPU utilization (system)']
@@ -181,12 +183,12 @@ def write_cpu_csv():
 
 if __name__ == "__main__":
     #Flags parser
-    parser = OptionParser('usage: -cs <path to context switches trace csv file> -cpu <path to CPU trace> -gc <path to GC trace> [-outcs <path to filtered context switches trace> -outcpu <path to filtered CPU trace>]')
-    parser.add_option('--cs', dest='cs_file', type='string', help="the context switches trace to be filtered")
-    parser.add_option('--cpu', dest='cpu_file', type='string', help="the CPU trace to be filtered")
-    parser.add_option('--gc', dest='gc_file', type='string', help="the GC trace on which the filtering of context switches and CPU data is based")
-    parser.add_option('--outcs', dest='out_cs_file', type='string', help="tthe name of trace containing the filtered context switches. If none is provided, then the trace will be named 'filtered-cs.csv'")
-    parser.add_option('--outcpu', dest='out_cpu_file', type='string', help="the name of the trace containing the filtered CPU measurements. If none is provided, then the trace file will be named 'filtered-cpu.csv'")
+    parser = OptionParser("Usage: ./gc-filtering.py -cs <path to CS trace> -cpu <path to CPU trace> -gc <path to GC trace> [-outcs <path to filtered CS trace (output)> -outcpu <path to filtered CPU trace (output)>]")
+    parser.add_option('--cs', dest='cs_file', type='string', help="path to the CS trace to be filtered", metavar="CS_TRACE")
+    parser.add_option('--cpu', dest='cpu_file', type='string', help="path to the CPU trace to be filtered", metavar="CPU_TRACE")
+    parser.add_option('--gc', dest='gc_file', type='string', help="path to the GC trace. Filtering of CS and CPU measurements are be based on data contained in this trace", metavar="GC_TRACE")
+    parser.add_option('--outcs', dest='out_cs_file', type='string', help="path to the output trace containing the filtered context switches. If none is provided, then the output trace will be produced in './filtered-cs.csv'", metavar="FILTERED_CS_TRACE")
+    parser.add_option('--outcpu', dest='out_cpu_file', type='string', help="path to the output trace containing the filtered CPU utilization measurements. If none is provided, then the output trace will be produced in './filtered-cpu.csv'", metavar="FILTERED_CPU_TRACE")
     (options, arguments) = parser.parse_args()
     if (options.cs_file is None):
         print(parser.usage)
@@ -230,9 +232,11 @@ if __name__ == "__main__":
     print("Number of context switches measurements after filtering: %s" % str(len(cs_data_array)))
     print("Number of CPU samplings after filtering: %s" % str(len(cpu_data_array)))
     print("")
-    print("Filtering successful")
+    print("Filtering complete.")
     print("")
 
     write_cs_csv()
 
     write_cpu_csv()
+
+
